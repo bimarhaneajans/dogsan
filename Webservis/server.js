e6a8d24e1387c97a6fec554dd116ee705c5e0564
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-04-06 02:05:15 +0300

    dd
    dddd

--- Webservis/server.js
+++ Webservis/server.js
@@ -2,12 +2,12 @@
 
 const http = require('http');
 
- let app = http.createServer((req, res) => {
+ let server = http.createServer((req, res) => {
      res.writeHead(200, {'Content-Type': 'text/plain'});
 
      res.end('server is run ! DOGSAN !\n');
 });
 
 // Start the server on port 3000
-app.listen(3000, '127.0.0.1');
+server.listen(3000, '127.0.0.1');
 console.log('Node server running on port 3000');
