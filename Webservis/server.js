5914a93b7cdf8ae036c038f9775fb77788ef2e3e
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-04-06 02:36:04 +0300

    dd
    ddd

--- Webservis/server.js
+++ Webservis/server.js
@@ -1,13 +1,54 @@
-// app.js
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const session = require('express-session');
 
-const http = require('http');
+const xXssProtection = require("x-xss-protection");
+const helmet = require("helmet");
+const hpp = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+var xssFilters = require('xss-filters');
+var csrf = require('csurf')
+var csrfProtection = csrf({ cookie: true })
 
- let server = http.createServer((req, res) => {
-     res.writeHead(200, {'Content-Type': 'text/plain'});
 
-     res.end('server is run ! DOGSAN !\n');
+
+
+const app = express();
+var corsOptions = {
+  origin: "http://status.madilink.net/"
+};
+
+app.use(cors(corsOptions));
+
+ app.use(bodyParser.json());
+
+ var expiryDate = new Date(Date.now() + 60 * 60 * 1000)
+ app.use(express.urlencoded({ extended: true }));
+
+
+ app.get("/", (req, res) => {
+  res.json({ message: "Dogsan" });
+});
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the reques>
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
 });
 
-// Start the server on port 3000
-server.listen(3000, '127.0.0.1');
-console.log('Node server running on port 3000');
+app.use(helmet.hidePoweredBy());
+app.use(hpp());
+app.use(xXssProtection());
+app.disable('x-powered-by');
+require("./routes/customer.routes")(app);
+
+ const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
