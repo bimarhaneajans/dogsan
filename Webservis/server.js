264b2d52182d070f2635d799174990876165d564
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-04-06 00:34:13 +0300

    dd
    dddd

--- Webservis/server.js
+++ Webservis/server.js
@@ -1,13 +1,87 @@
-// app.js
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+ const mongoose = require('mongoose');
 
-const http = require('http');
+const app = express();
 
- let app = http.createServer((req, res) => {
-     res.writeHead(200, {'Content-Type': 'text/plain'});
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
 
-     res.end('Hello World!\n');
+app.use(cors(corsOptions));
+
+ app.use(bodyParser.json());
+
+ app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+const Role = db.role;
+require("./app/routes/turorial.routes")(app);
+
+mongoose
+  .connect("mongodb+srv://", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+  app.get("/", (req, res) => {
+  res.json({ message: "Dogsan Server Aktif" });
+}); 
+
+// routes
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+const server=app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
 
-// Start the server on port 3000
-app.listen(3000, '127.0.0.1');
-console.log('Node server running on port 3000');
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      }); 
+
+    }
+
+    
+  });
+}
