ba4564bf2d4205fc6376415682da369d6a66b369
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-04-12 12:44:36 +0300

    dd
    dddd

--- Webservis/server.js
+++ Webservis/server.js
@@ -1,88 +1,84 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
-const session = require('express-session');
+const cookieSession = require("cookie-session");
 
-const xXssProtection = require("x-xss-protection");
-const helmet = require("helmet");
-const hpp = require("xss-clean");
-const rateLimit = require("express-rate-limit");
-var xssFilters = require('xss-filters');
-var csrf = require('csurf')
-var csrfProtection = csrf({ cookie: true })
 const dbConfig = require("./src/config/db.config");
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the reques>
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  } else {
-    next();
-  }
-});
-
-app.use(helmet.hidePoweredBy());
-app.use(hpp());
-app.use(xXssProtection());
-app.disable('x-powered-by');
-
 const app = express();
+
 var corsOptions = {
-  origin: "http://37.77.4.139:3000"
+  origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
 
- app.use(bodyParser.json());
+// parse requests of content-type - application/json
+app.use(express.json());
 
- var expiryDate = new Date(Date.now() + 60 * 60 * 1000)
- app.use(express.urlencoded({ extended: true }));
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
+  app.use(
+  cookieSession({
+    name: "dogsan-session",
+    secret: "dogsanwebsite", // should use as secret environment variable
+    httpOnly: true
+  })
+); 
 
- app.get("/", (req, res) => {
-  res.json({ message: "Dogsan" });
-});
+const db = require("./src/models");
+const Role = db.role;
 
- //db.sequelize.sync({force: true})
- const db = require("./src/models");
- const Role = db.role;
-
- db.mongoose
-  .connect(db.url, {
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
   .then(() => {
-    console.log("Connected to the database!");
+    console.log("Successfully connect to MongoDB.");
+    initial();
   })
   .catch(err => {
-    console.log("Cannot connect to the database!", err);
+    console.error("Connection error", err);
     process.exit();
   });
-  
-/*  db.sequelize.sync({force: true}).then(() => {
-  console.log('Drop and Resync Database with { force: true }');
-  initial();
- }); */
- 
 
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Dogsan" });
+});
+
+// routes
 require("./src/routes/auth.routes")(app);
 require("./src/routes/user.routes")(app);
-  const PORT = process.env.PORT || 3000;
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-
 function initial() {
-  Role.create({
-    id: 2,
-    name: "user"
-  }); 
-  Role.create({
-    id: 1,
-    name: "admin"
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      }); 
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
   });
 }
