f4895bb590d5361b8ea4358ffd6ee1676a0357ad
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-04-19 03:06:04 +0300

    ddd
    dddd

--- Website/dogsan/src/services/auth-header.js
+++ Website/dogsan/src/services/auth-header.js
@@ -1,11 +1,7 @@
 export default function authHeader() {
   const user = JSON.parse(localStorage.getItem("user"));
 
-  if (user && user.accessToken) {
-    // For Spring Boot back-end
-    // return { Authorization: "Bearer " + user.accessToken };
-
-    // for Node.js Express back-end
+  if (user && user.accessToken) { 
     return { "x-access-token": user.accessToken };
   } else {
     return {};
