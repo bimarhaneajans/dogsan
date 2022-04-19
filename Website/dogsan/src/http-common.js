581e7e813749ab7d084a14cbc4ff8934b883557e
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-04-19 14:50:35 +0300

    dd
    ddd

--- Website/dogsan/src/http-common.js
+++ Website/dogsan/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://bavrim.madilink.net/api",
   headers: {
     "Content-type": "application/json"
   }
