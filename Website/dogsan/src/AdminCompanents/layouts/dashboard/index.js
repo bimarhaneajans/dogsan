814e0178dd95f000ad936e21f554c508266ab937
nur81 <100799851+nur81@users.noreply.github.com>
2022-04-22 02:15:47 +0300

    cc

--- Website/dogsan/src/AdminCompanents/layouts/dashboard/index.js
+++ Website/dogsan/src/AdminCompanents/layouts/dashboard/index.js
@@ -3,29 +3,29 @@ import Grid from "@mui/material/Grid";
 import Icon from "@mui/material/Icon";
 
 // Soft UI Dashboard React components
-import SuiBox from "./components/SuiBox";
-import SuiTypography from "./components/SuiTypography";
+import SuiBox from "../../components/SuiBox";
+import SuiTypography from "../../components/SuiTypography";
 
 // Soft UI Dashboard React ./examples
-import DashboardLayout from "././examples/LayoutContainers/DashboardLayout";
-import DashboardNavbar from "././examples/Navbars/DashboardNavbar";
-import Footer from "././examples/Footer";
-import MiniStatisticsCard from "././examples/Cards/StatisticsCards/MiniStatisticsCard";
-import ReportsBarChart from "././examples/Charts/BarCharts/ReportsBarChart";
-import GradientLineChart from "././examples/Charts/LineCharts/GradientLineChart";
+import DashboardLayout from "../../examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "../../examples/Navbars/DashboardNavbar";
+import Footer from "../../examples/Footer";
+import MiniStatisticsCard from "../../examples/Cards/StatisticsCards/MiniStatisticsCard";
+import ReportsBarChart from "../../examples/Charts/BarCharts/ReportsBarChart";
+import GradientLineChart from "../../examples/Charts/LineCharts/GradientLineChart";
 
 // Soft UI Dashboard React base styles
-import typography from "./assets/theme/base/typography";
+import typography from "../../assets/theme/base/typography";
 
 // Dashboard layout components
-import BuildByDevelopers from "././layouts/dashboard/components/BuildByDevelopers";
-import WorkWithTheRockets from "././layouts/dashboard/components/WorkWithTheRockets";
-import Projects from "././layouts/dashboard/components/Projects";
-import OrderOverview from "././layouts/dashboard/components/OrderOverview";
+import BuildByDevelopers from "../../layouts/dashboard/components/BuildByDevelopers";
+import WorkWithTheRockets from "../../layouts/dashboard/components/WorkWithTheRockets";
+import Projects from "../../layouts/dashboard/components/Projects";
+import OrderOverview from "../../layouts/dashboard/components/OrderOverview";
 
 // Data
-import reportsBarChartData from "././layouts/dashboard/data/reportsBarChartData";
-import gradientLineChartData from "././layouts/dashboard/data/gradientLineChartData";
+import reportsBarChartData from "../../layouts/dashboard/data/reportsBarChartData";
+import gradientLineChartData from "../../layouts/dashboard/data/gradientLineChartData";
 
 function Dashboard() {
   const { size } = typography;
