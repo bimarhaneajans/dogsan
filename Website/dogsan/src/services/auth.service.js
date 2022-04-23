9ec7be3ddba9c63e20f832a3636eab55c7b9d0b2
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-04-23 16:57:30 +0300

    d
    dd

--- Website/dogsan/src/services/auth.service.js
+++ Website/dogsan/src/services/auth.service.js
@@ -1,8 +1,5 @@
 import axios from "axios";
- //const API_URL = "https://46.4.116.218:3000/api/auth/";
- const API_URL = "https://bavrim.madilink.net/api/auth/";
-
-//https://46.4.116.218:3000/api/test/
+  const API_URL = "https://bavrim.madilink.net/api/auth/";
  
 class AuthService {
   login(username, password) {
