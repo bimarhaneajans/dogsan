6456bfdd8fd59128c55809e6a316c1826299d28b
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-04-24 03:17:08 +0300

    dd
    ddd

--- Website/dogsan/src/index.js
+++ Website/dogsan/src/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import store from "./store";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import store, { persistor } from "./redux/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can chađinge
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
