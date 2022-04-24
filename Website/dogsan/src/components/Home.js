51322a1b0186fa04279a712753f4259a6a2a8006
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-04-24 22:46:58 +0300

    dd
    dddd

--- Website/dogsan/src/components/Home.js
+++ Website/dogsan/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import '../css/styles.css';
 import UserService from "../services/user.service";
 
 function Home() {
@@ -23,36 +23,34 @@ function Home() {
 
   return (
     <div className="container">
-      <header className="jumbotron">
-       {/*  <h3>{content}</h3> */}
-      </header>
-      <img src="healthcare-medical-concept-med.png"/>
+      
+      <img src="../healthcare-medical-concept-med.png"/>
    <div />
-   <div id="emilebilir" class="emilebilir">
+   <div id="emilebilir" className="emilebilir">
 
     <div id="EMLEBLR_STRLER">
      <span>EMİLEBİLİR<br />SÜTÜRLER</span>
     </div>
    </div>
-   <div id="milmeyen" class="milmeyen">
+   <div id="milmeyen" className="milmeyen">
 
     <div id="EMLMEYEN_STRLER">
      <span>EMİLMEYEN<br />SÜTÜRLER</span>
     </div>
    </div>
-   <div id="Component_7__1" class="Component_7___1">
+   <div id="Component_7__1" className="Component_7___1">
 
     <div id="EMLEBLR_HEMOSTAT">
      <span>EMİLEBİLİR<br />HEMOSTAT</span>
     </div>
    </div>
-   <div id="Component_8__1" class="Component_8___1">
+   <div id="Component_8__1" className="Component_8___1">
 
     <div id="DENTAL_STRLER">
      <span>DENTAL<br />SÜTÜRLER</span>
     </div>
    </div>
-   <div id="Component_9__1" class="Component_9___1">
+   <div id="Component_9__1" className="Component_9___1">
 
     <div id="VETERNERLK">
      <span>VETERİNERLİK</span>
