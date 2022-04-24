1dac72d71e2a2fb73b014f38ac82134108079602
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-04-24 03:22:01 +0300

    dd
    dddd

--- Website/dogsan/src/store.js
+++ Website/dogsan/src/store.js
@@ -2,12 +2,20 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { persistStore, persistReducer } from "redux-persist";
+import storageSession from "redux-persist/es/storage/session";
+
+const persistConfig = {
+  key: "root",
+  storage: storageSession,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(...middleware)));
+
+export const persistor = persistStore(store);
 
 export default store;
