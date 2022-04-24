96a129fedc47dc1321e929cafbeac91b5efae6da
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-04-24 03:31:42 +0300

    dd
    ddd

--- Website/dogsan/src/index.js
+++ Website/dogsan/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import store, { persistor } from "./redux/store";
+import store, { persistor } from "./store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
--- Website/dogsan/src/reducers/index.js
+++ Website/dogsan/src/reducers/index.js
@@ -1,11 +1,10 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
 import message from "./message";
-import adminreducer from "./adminreducer";
-
+import admin from "./admin";
 
 export default combineReducers({
   auth,
   message,
-  //adminreducer
+  admin,
 });
