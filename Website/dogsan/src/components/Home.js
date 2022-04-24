7d5a082433d7e7cde93cc2bd41d047aeee835383
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-04-24 22:37:26 +0300

    dd
    dd

--- Website/dogsan/src/components/Home.js
+++ Website/dogsan/src/components/Home.js
@@ -24,8 +24,40 @@ function Home() {
   return (
     <div className="container">
       <header className="jumbotron">
-        <h3>{content}</h3>
+       {/*  <h3>{content}</h3> */}
       </header>
+      <img src="healthcare-medical-concept-med.png"/>
+   <div />
+   <div id="emilebilir" class="emilebilir">
+
+    <div id="EMLEBLR_STRLER">
+     <span>EMİLEBİLİR<br />SÜTÜRLER</span>
+    </div>
+   </div>
+   <div id="milmeyen" class="milmeyen">
+
+    <div id="EMLMEYEN_STRLER">
+     <span>EMİLMEYEN<br />SÜTÜRLER</span>
+    </div>
+   </div>
+   <div id="Component_7__1" class="Component_7___1">
+
+    <div id="EMLEBLR_HEMOSTAT">
+     <span>EMİLEBİLİR<br />HEMOSTAT</span>
+    </div>
+   </div>
+   <div id="Component_8__1" class="Component_8___1">
+
+    <div id="DENTAL_STRLER">
+     <span>DENTAL<br />SÜTÜRLER</span>
+    </div>
+   </div>
+   <div id="Component_9__1" class="Component_9___1">
+
+    <div id="VETERNERLK">
+     <span>VETERİNERLİK</span>
+    </div>
+   </div>
     </div>
   );
 };
