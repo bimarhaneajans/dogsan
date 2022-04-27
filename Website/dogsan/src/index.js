1a7756db2070d75350111704b21184ef38087ca7
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-04-27 13:08:59 +0300

    dd
    dd

--- Website/dogsan/src/index.js
+++ Website/dogsan/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "App";
+import App from "./Admin";
 import store, { persistor } from "./store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
