d75fe3a84e15256ea6fbc33aa3b96f3c51efdfad
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-05-02 20:26:46 +0300

    dd
    ddd

--- dogsan/src/admin/Admin.js
+++ dogsan/src/admin/Admin.js
@@ -45,7 +45,40 @@ import Bayiekle from "../layouts/Bayi/Bayiekle"
 import Bayiliste from "../layouts/Bayi/bayiliste"
 import Bayiguncellle from "../layouts/Bayi/Bayiguncelle"
 import Dashboard from "layouts/dashboard";
-import Home from "../components/Home"
+ import Login from "../components/Login";
+import Register from "../components/Register";
+import Home from "../components/Home";
+import Profile from "../components/Profile";
+import Header from "../components/header/header";
+import Slider from "../components/slider/slider";
+import Navbar from "../components/navbar/navbar";
+import NavbarHome from "../components/navbar/NavbarHome";
+import Footer from "../components/footer/footer";
+import Kataloglar from "../components/kataloglar/kataloglar";
+import Kvc from "../components/kvc/kvc";
+import Kvc2 from "../components/kvc/kvc2";
+import EmilebilirSuturler2 from "../components/EmilebilirSuturler/EmilebilirSuturler2";
+import EmilebilirSuturler from "../components/EmilebilirSuturler/EmilebilirSuturler";
+import EmilmeyenSuturler from "../components/EmilmeyenSuturler/EmilmeyenSuturler";
+import EmilmeyenSuturler2 from "../components/EmilmeyenSuturler/EmilmeyenSuturler2";
+import EmilebilirHemostat from "../components/EmilebilirHemostat/EmilebilirHemostat";
+import EmilebilirHemostat2 from "../components/EmilebilirHemostat/EmilebilirHemostat2";
+import DentalSuturler from "../components/DentalSuturler/DentalSuturler";
+import DentalSuturler2 from "../components/DentalSuturler/DentalSuturler2";
+import Veterinerlik from "../components/Veterinerlik/Veterinerlik";
+import Veterinerlik2 from "../components/Veterinerlik/Veterinerlik";
+import HeaderKurumsal from "../components/kurumsal/header/HeaderKurumsal";
+import Kurumsal from "../components/kurumsal/kurumsal";
+import Hakkimizda from  "../components/kurumsal/hakkimizda";
+import Degerler from  "../components/kurumsal/degerler";
+import SosyalSorumluluk from  "../components/kurumsal/SosyalSorumluluk";
+import Tarihce from "../components/kurumsal/tarihce";
+import Igneler from  "../components/igneler/igneler";
+import "../assets/assets/css/style.css"
+import "../assets/assets/css/responsive-styling.css"
+import "../assets/assets/vendor/owl-carousel/owl-carousel/owl.carousel.css"
+import "../assets/assets/vendor/owl-carousel/owl-carousel/owl.theme.css"
+import "../assets/assets/vendor/bootstrap/css/bootstrap.min.css"
 
 // Soft UI Dashboard React contexts
 import { useSoftUIController, setMiniSidenav, setOpenConfigurator } from "context";
@@ -137,7 +170,39 @@ export default function App() {
         {getRoutes(routes)} 
         
       </Routes>    */}
-       <Home/> 
+      {/*  <Home/>  */}
+
+         <Routes>
+         <Route exact path="/" element={<Home />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/kvc" element={<Kvc />} />
+          <Route exact path="/kvc2" element={<Kvc2/>} />
+          <Route exact path="/EmilebilirSuturler" element={<EmilebilirSuturler />} />
+          <Route exact path="/EmilebilirSuturler2" element={<EmilebilirSuturler2/>} />
+          <Route exact path="/EmilmeyenSuturler" element={<EmilmeyenSuturler />} />
+          <Route exact path="/EmilmeyenSuturler2" element={<EmilmeyenSuturler2/>} />
+          <Route exact path="/EmilebilirHemostat" element={<EmilebilirHemostat />} />
+          <Route exact path="/EmilebilirHemostat2" element={<EmilebilirHemostat2/>} />
+          <Route exact path="/DentalSuturler" element={<DentalSuturler />} />
+          <Route exact path="/DentalSuturler2" element={<DentalSuturler2/>} />
+          <Route exact path="/Veterinerlik" element={<Veterinerlik />} />
+          <Route exact path="/Veterinerlik2" element={<Veterinerlik2/>} />
+          <Route path="/Kataloglar" element={<Kataloglar />} />
+          <Route path="/HeaderKurumsal" element={<HeaderKurumsal />} />
+          <Route path="/Kurumsal" element={<Kurumsal />} />
+          <Route path="/hakkimizda" element={<Hakkimizda />} />
+          <Route path="/Degerler" element={<Degerler />} />
+          <Route path="/SosyalSorumluluk" element={<SosyalSorumluluk />} />
+          <Route path="/Tarihce" element={<Tarihce />} />
+          <Route path="/Igneler" element={<Igneler />} />
+         
+ 
+    {/*       <Route path="/user" element={<BoardUser />} />
+          <Route path="/mod" element={<BoardModerator />} />
+          
+          <Route path="/admin" element={<BoardAdmin />} /> */}
+        </Routes>
     </ThemeProvider>
     
   );
