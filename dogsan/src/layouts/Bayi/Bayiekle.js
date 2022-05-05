43438cbfc3a33f53d8aad450562553e439aeb1e4
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-05-05 05:21:58 +0300

    ddd
    dddd

--- dogsan/src/layouts/Bayi/Bayiekle.js
+++ dogsan/src/layouts/Bayi/Bayiekle.js
@@ -1,5 +1,5 @@
 
-import React, {useState,useEffect,useMemo, useRef  } from "react";
+import React, { useState, useEffect, useMemo, useRef } from "react";
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { useParams, useNavigate } from 'react-router-dom';
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
@@ -10,7 +10,9 @@ import typography from "assets/theme/base/typography";
 import Sidenav from "examples/Sidenav";
 import routes from "../../routes";
 import { Link } from "react-router-dom";
- 
+import { Editor } from "react-draft-wysiwyg";
+import { convertFromRaw } from 'draft-js';
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import brand from "assets/images/logo-ct.png";
 
 const BayiEkle = () => {
@@ -27,12 +29,12 @@ const BayiEkle = () => {
 
   const [tutorial, setTutorial] = useState(initialTutorialState);
   const [submitted, setSubmitted] = useState(false);
-   const [currentTutorial, setCurrentTutorial] = useState(null);
+  const [currentTutorial, setCurrentTutorial] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(-1);
   const [searchTitle, setSearchTitle] = useState("");
-   const [controller, dispatch] = useSoftUIController();
+  const [controller, dispatch] = useSoftUIController();
   const { miniSidenav, direction, layout, openConfigurator, sidenavColor } = controller;
- 
+
   const [onMouseEnter, setOnMouseEnter] = useState(false);
   const [rtlCache, setRtlCache] = useState(null);
   const { pathname } = useLocation();
@@ -77,98 +79,110 @@ const BayiEkle = () => {
   };
 
   return (
-    <DashboardLayout> 
-       <Sidenav
-            color={sidenavColor}
-            brand={brand}
-            brandName=" DOĞSAN PANEL "
-            routes={routes} 
-          />
-    <div style={{ marginLeft: "100px" }}> 
-      <Header />
-    </div>
-
-    <div style={{ width: "300px", marginLeft: "100px" }}>
-      <div className="submit-form">
-        {submitted ? (
-          <div>
-            <h4>Başarılı! Yeni eklemek istermisin ?</h4>
-            <button className="btn btn-success" onClick={newTutorial}>
-              Ekle
-            </button>
-          </div>
-        ) : (
-          <div>
-            <div className="form-group">
-              <label htmlFor="bayi">Başlık</label>
-              <input
-                type="text"
-                className="form-control"
-                id="baslik"
-                required
-                value={tutorial.baslik}
-                onChange={handleInputChange}
-                name="baslik"
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="adres">adres</label>
-              <input
-                type="text"
-                className="form-control"
-                id="adres"
-                required
-                value={tutorial.adres}
-                onChange={handleInputChange}
-                name="adres"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="Telefon">Telefon</label>
-              <input
-                type="text"
-                className="form-control"
-                id="telefon"
-                required
-                value={tutorial.telefon}
-                onChange={handleInputChange}
-                name="telefon"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="Enlem">Enlem</label>
-              <input
-                type="text"
-                className="form-control"
-                id="enlem"
-                required
-                value={tutorial.enlem}
-                onChange={handleInputChange}
-                name="enlem"
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="boylam">boylam</label>
-              <input
-                type="text"
-                className="form-control"
-                id="boylam"
-                required
-                value={tutorial.boylam}
-                onChange={handleInputChange}
-                name="boylam"
-              />
-            </div>
-
-
-            <button onClick={saveTutorial} className="btn btn-success">
-              Submit
-            </button>
-          </div>
-        )}
+    <DashboardLayout>
+      <Sidenav
+        color={sidenavColor}
+        brand={brand}
+        brandName=" DOĞSAN PANEL "
+        routes={routes}
+      />
+      <div style={{ marginLeft: "100px" }}>
+        <Header />
       </div>
+
+      <div style={{ width: "300px", marginLeft: "100px" }}>
+        <div className="submit-form">
+          {submitted ? (
+            <div>
+              <h4>Başarılı! Yeni eklemek istermisin ?</h4>
+              <button className="btn btn-success" onClick={newTutorial}>
+                Ekle
+              </button>
+            </div>
+          ) : (
+            <div>
+              <div className="form-group">
+                <label htmlFor="bayi">Başlık</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="baslik"
+                  required
+                  value={tutorial.baslik}
+                  onChange={handleInputChange}
+                  name="baslik"
+                />
+              </div>
+
+              <div style={{width:"300 px"}}>
+              <Editor
+               editorState={tutorial.boylam}
+                toolbarClassName="toolbarClassName"
+                wrapperClassName="wrapperClassName"
+                editorClassName="editorClassName"
+                onEditorStateChange={handleInputChange}
+              />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="adres">adres</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="adres"
+                  required
+                  value={tutorial.adres}
+                  onChange={handleInputChange}
+                  name="adres"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="Telefon">Telefon</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="telefon"
+                  required
+                  value={tutorial.telefon}
+                  onChange={handleInputChange}
+                  name="telefon"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="Enlem">Enlem</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="enlem"
+                  required
+                  value={tutorial.enlem}
+                  onChange={handleInputChange}
+                  name="enlem"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="boylam">boylam</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="boylam"
+                  required
+                  value={tutorial.boylam}
+                  onChange={handleInputChange}
+                  name="boylam"
+                />
+              </div>
+              
+             
+
+
+              <button onClick={saveTutorial} className="btn btn-success">
+                Submit
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </DashboardLayout>
   );
