8d7bfc9ff95d7c03f5c56c7c872b7f15c6682941
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-05-11 08:13:00 +0300

    dd
    dddd

---
+++ dogsan/src/layouts/Yoneticiler/Bayiekle.js
@@ -0,0 +1,243 @@
+
+import React, { useState, useEffect, useMemo, useRef } from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { useParams, useNavigate } from 'react-router-dom';
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import YoneticilerDataService from "../../services/YoneticilerService";
+import { useSoftUIController, setMiniSidenav, setOpenConfigurator } from "context";
+import Header from "layouts/profile/components/Header";
+import typography from "assets/theme/base/typography";
+import Sidenav from "examples/Sidenav";
+import routes from "../../routes";
+import { Link } from "react-router-dom";
+import { Editor } from "react-draft-wysiwyg";
+import { convertFromRaw } from 'draft-js';
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import brand from "assets/images/logo-ct.png";
+import FileBase64 from 'react-file-base64';
+
+const BayiEkle = () => {
+  const initialTutorialState = {
+    id: null,
+    baslik: "",
+    yoneticiadi: "",
+    yoneticisoyadi: "",
+    kariyer: "",
+    pozizyon: "",
+    twitter: "",
+    facebook: "",
+    linkedin: "",
+    googleplus: "",
+    published: false
+  };
+
+
+  const [tutorial, setTutorial] = useState(initialTutorialState);
+  const [submitted, setSubmitted] = useState(false);
+  const [currentTutorial, setCurrentTutorial] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const [searchTitle, setSearchTitle] = useState("");
+  const [controller, dispatch] = useSoftUIController();
+  const { miniSidenav, direction, layout, openConfigurator, sidenavColor } = controller;
+
+  const [onMouseEnter, setOnMouseEnter] = useState(false);
+  const [rtlCache, setRtlCache] = useState(null);
+  const { pathname } = useLocation();
+  const { size } = typography;
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setTutorial({ ...tutorial, [name]: value });
+  };
+
+  const saveTutorial = () => {
+    var data = {
+      yoneticiadi: tutorial.yoneticiadi,
+      yoneticisoyadi: tutorial.yoneticisoyadi,
+      kariyer: tutorial.kariyer,
+      pozizyon: tutorial.pozizyon,
+      twitter: tutorial.twitter,
+      facebook: tutorial.facebook,
+      linkedin: tutorial.linkedin,
+      googleplus: tutorial.googleplus,
+      Resim: tutorial.Resim,
+    };
+
+    YoneticilerDataService.create(data)
+      .then(response => {
+        setTutorial({
+          id: response.data.id,
+          yoneticiadi: response.data.yoneticiadi,
+          yoneticisoyadi: response.data.yoneticisoyadi,
+          kariyer: response.data.kariyer,
+          pozizyon: response.data.pozizyon,
+          twitter: response.data.twitter,
+          facebook: response.data.facebook,
+          linkedin: response.data.linkedin,
+          googleplus: response.data.googleplus,
+          Resim: response.data.Resim,
+          published: response.data.published
+        });
+        setSubmitted(true);
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const newTutorial = () => {
+    setTutorial(initialTutorialState);
+    setSubmitted(false);
+  };
+
+  return (
+    <DashboardLayout>
+      <Sidenav
+        color={sidenavColor}
+        brand={brand}
+        brandName=" DOĞSAN PANEL "
+        routes={routes}
+      />
+      <div style={{ marginLeft: "100px" }}>
+        <Header />
+      </div>
+
+      <div style={{ width: "300px", marginLeft: "100px" }}>
+        <div className="submit-form">
+          {submitted ? (
+            <div>
+              <h4>Başarılı! Yeni eklemek istermisin ?</h4>
+              <button className="btn btn-success" onClick={newTutorial}>
+                Ekle
+              </button>
+            </div>
+          ) : (
+            <div>
+              <Editor style={{ width: 600 }}
+                editorState={tutorial.baslik}
+                toolbarClassName="toolbarClassName"
+                wrapperClassName="wrapperClassName"
+                editorClassName="editorClassName"
+                onEditorStateChange={handleInputChange}
+              />
+              <div className="form-group">
+                <label htmlFor="bayi">yoneticiadi</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="yoneticiadi"
+                  required
+                  value={tutorial.yoneticiadi}
+                  onChange={handleInputChange}
+                  name="yoneticiadi"
+                />
+
+              </div>
+
+
+
+              <div className="form-group">
+                <label htmlFor="adres">yoneticisoyadi</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="yoneticisoyadi"
+                  required
+                  value={tutorial.yoneticisoyadi}
+                  onChange={handleInputChange}
+                  name="yoneticisoyadi"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="kariyer">kariyer</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="kariyer"
+                  required
+                  value={tutorial.kariyer}
+                  onChange={handleInputChange}
+                  name="kariyer"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="pozizyon">pozizyon</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="pozizyon"
+                  required
+                  value={tutorial.pozizyon}
+                  onChange={handleInputChange}
+                  name="pozizyon"
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="twitter">twitter</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="twitter"
+                  required
+                  value={tutorial.twitter}
+                  onChange={handleInputChange}
+                  name="twitter"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="boylam">facebook</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="facebook"
+                  required
+                  value={tutorial.facebook}
+                  onChange={handleInputChange}
+                  name="facebook"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="boylam">linkedin</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="linkedin"
+                  required
+                  value={tutorial.linkedin}
+                  onChange={handleInputChange}
+                  name="linkedin"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="boylam">googleplus</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="googleplus"
+                  required
+                  value={tutorial.googleplus}
+                  onChange={handleInputChange}
+                  name="googleplus"
+                />
+              </div>
+
+              <FileBase64
+                type="file"
+                multiple={false}
+                onDone={({ base64 }) => setTutorial({ ...tutorial, Resim: base64 })}
+              />
+
+              <button onClick={saveTutorial} className="btn btn-success">
+                Submit
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default BayiEkle;
---
+++ dogsan/src/layouts/Yoneticiler/bayiliste.js
@@ -0,0 +1,211 @@
+
+import React, { useState, useEffect, useMemo, useRef } from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { useParams, useNavigate } from 'react-router-dom';
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import BayiDataService from "../../services/BayiService";
+import { useSoftUIController, setMiniSidenav, setOpenConfigurator } from "context";
+import Header from "layouts/profile/components/Header";
+import typography from "assets/theme/base/typography";
+import Sidenav from "examples/Sidenav";
+import routes from "../../routes";
+import { Link } from "react-router-dom";
+import brand from "assets/images/logo-ct.png";
+import FileBase64 from 'react-file-base64';
+
+const Overview = (props) => {
+  const [tutorials, setTutorials] = useState([]);
+  const [currentTutorial, setCurrentTutorial] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const [searchTitle, setSearchTitle] = useState("");
+  const [controller, dispatch] = useSoftUIController();
+  const { miniSidenav, direction, layout, openConfigurator, sidenavColor } = controller;
+  const [onMouseEnter, setOnMouseEnter] = useState(false);
+  const [rtlCache, setRtlCache] = useState(null);
+  const { pathname } = useLocation();
+  const { size } = typography;
+
+  useEffect(() => {
+    retrieveTutorials();
+  }, []);
+
+  const onChangeSearchTitle = e => {
+    const searchTitle = e.target.value;
+    setSearchTitle(searchTitle);
+  };
+
+  const retrieveTutorials = () => {
+    BayiDataService.getAll()
+      .then(response => {
+        setTutorials(response.data);
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const refreshList = () => {
+    retrieveTutorials();
+    setCurrentTutorial(null);
+    setCurrentIndex(-1);
+  };
+
+  const setActiveTutorial = (tutorial, index) => {
+    setCurrentTutorial(tutorial);
+    setCurrentIndex(index);
+  };
+
+  const removeAllTutorials = () => {
+    BayiDataService.removeAll()
+      .then(response => {
+        console.log(response.data);
+        refreshList();
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const findByTitle = () => {
+    BayiDataService.findByTitle(searchTitle)
+      .then(response => {
+        setTutorials(response.data);
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <DashboardLayout>
+      <Sidenav
+        color={sidenavColor}
+        brand={brand}
+        brandName=" DOĞSAN PANEL "
+        routes={routes}
+      />
+      <div style={{ marginLeft: "100px" }}>
+        <Header />
+      </div>
+
+      <div style={{ width: "300px", marginLeft: "275px", marginTop: "20px" }}>
+
+        <div className="list row">
+          <div className="col-md-8">
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Arama" 
+                value={searchTitle}
+                onChange={onChangeSearchTitle}
+              />
+              <div className="input-group-append">
+                <button
+                  className="btn btn-outline-secondary"
+                  type="button"
+                  onClick={findByTitle}
+                >
+                  Ara
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+
+
+            <ul className="list-group">
+              {tutorials &&
+                tutorials.map((tutorial, index) => (
+                  <li
+                    className={
+                      "list-group-item " + (index === currentIndex ? "active" : "")
+                    }
+                    onClick={() => setActiveTutorial(tutorial, index)}
+                    key={index}
+                  >
+                    <div className="card" key={tutorial.id}>
+                    <div className="card-image waves-effect waves-block waves-light">
+                        <img className="activator" style={{ width: '100%', height: 150 }} src={tutorial.Resim} />
+                    </div>
+                    <div className="card-content">
+                        <span className="card-title activator grey-text text-darken-4">{tutorial.baslik}</span>
+                    </div>
+                </div>
+                  </li>
+                ))}
+            </ul>
+
+            <button
+              className="m-3 btn btn-sm btn-danger"
+              onClick={removeAllTutorials}
+            >
+              Tümünü Sil
+            </button>
+          </div>
+          <div className="col-md-6">
+            {currentTutorial ? (
+              <div>
+                <strong>Başlık:</strong>
+                <div>
+                  <label>
+
+                  </label>{" "}
+                  {currentTutorial.baslik}
+                </div>
+                <div>
+                  <label>
+                    <strong>Adres:</strong>
+                  </label>{" "}
+                  {currentTutorial.adres}
+                </div>
+                <div>
+                  <label>
+                    <strong>Enlem:</strong>
+                  </label>{" "}
+                  {currentTutorial.enlem}
+                </div>
+                <div>
+                  <label>
+                    <strong>Telefon:</strong>
+                  </label>{" "}
+                  {currentTutorial.telefon}
+                </div>
+                <div>
+                  <label>
+                    <strong>Boylam:</strong>
+                  </label>{" "}
+                  {currentTutorial.boylam}
+                </div>
+                <div>
+                  <label>
+                    <strong>Durum:</strong>
+                  </label>{" "}
+                  {currentTutorial.published ? "Published" : "Pending"}
+                </div>
+
+
+            
+                <Link
+                  to={"/bayiguncelle/" + currentTutorial.id}
+                  className="m-6 btn btn-lm btn-warning"
+                >
+                  Düzenle
+                </Link>
+              </div>
+            ) : (
+              <div>
+                <br />
+              
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default Overview;
