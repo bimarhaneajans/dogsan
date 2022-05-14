d2433152aa1fa5f21b05a88fb6e2bc3d11ef6940
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-05-14 16:05:50 +0300

    ddd
    ddd

---
+++ dogsan/src/sliders/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return(
+    <header>
+      <section>
+        <img src="http://svgshare.com/i/40g.svg" alt="W Logo" className="logo" />
+        <a href="https://github.com/erichbehrens/react-animated-slider" target="_blank">
+          <img src="http://svgshare.com/i/3zm.svg" alt="GitHub Repository" />
+        </a>
+      </section>
+    </header>
+  )
+}
+
+export default Header;
