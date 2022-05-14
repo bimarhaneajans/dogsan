f3be0f67c233d6d45b8ce77a89cb1e4dd9a4e396
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-05-14 16:56:42 +0300

    dd
    ddd

--- dogsan/src/sliders/header.js
+++ dogsan/src/sliders/header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
 function Header() {
-  return(
-    <header>
-      <section>
-        <img src="http://svgshare.com/i/40g.svg" alt="W Logo" className="logo" />
-        <a href="https://github.com/erichbehrens/react-animated-slider" target="_blank">
-          <img src="http://svgshare.com/i/3zm.svg" alt="GitHub Repository" />
-        </a>
-      </section>
-    </header>
-  )
+	return (
+		<header>
+			<section>
+				<h1>
+					React Animated Slider
+					<a href="https://github.com/erichbehrens/react-animated-slider" target="_blank">
+						<img src="https://svgshare.com/i/3zm.svg" alt="GitHub Repository" />
+					</a>
+				</h1>
+
+			</section>
+		</header>
+	);
 }
 
 export default Header;
