709b1d67cf4de0cdd7be40034cc97c4b460f19f6
nur81 <100799851+nur81@users.noreply.github.com>
2022-05-15 14:09:10 +0300

    gggg
    ggggg

--- dogsan/src/layouts/Home.js
+++ dogsan/src/layouts/Home.js
@@ -57,7 +57,7 @@ import "./social.css"
 import Subdynamicdetaykategori from "../layouts/Kategori/subdynamicdetaykategori"
 import EmilebilirSuturler from "./EmilebilirSuturler/EmilebilirSuturler";
 import SinglePost from "./SinglePost";
-import Hakkimizda from "./kurumsal/hakkimizda";
+import Hakkimizda from "./kurumsal/Hakkimizda";
 
 
 
