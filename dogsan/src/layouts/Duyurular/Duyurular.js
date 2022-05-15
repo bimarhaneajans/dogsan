3af97b7ca2055243e8270b85057325aa8c7abe4c
nur81 <100799851+nur81@users.noreply.github.com>
2022-05-15 19:27:55 +0300

    gggg
    ggggg

--- dogsan/src/layouts/Duyurular/Duyurular.js
+++ dogsan/src/layouts/Duyurular/Duyurular.js
@@ -3,7 +3,8 @@
 import React, { useState, useEffect, useMemo, useRef } from "react";
 import { Routes, Route, Navigate, useLocation, useParams, useNavigate, Link } from "react-router-dom";
 import { useSoftUIController, setMiniSidenav, setOpenConfigurator } from "context";
- import typography from "../../assets/theme/base/typography";
+
+import typography from "../../assets/theme/base/typography";
 import DuyuruDataService from "../../services/EtkinlikService";
 import logo from "../assets/img/logo/heartify-logo.png";
 import logo2 from "../assets/img/logo/heartify-logo-lite.png";
@@ -15,17 +16,15 @@ import "../assets/vendor/owl-carousel/owl-carousel/owl.carousel.css";
 import "../assets/vendor/owl-carousel/owl-carousel/owl.theme.css";
 import "../style.css";
 import "../responsive-styling.css";
- import Timeline from "./DuyuruTimeline.js"
 import Kataloglar from "layouts/kataloglar/Kataloglar";
- 
+
 import { Player, ControlBar } from 'video-react';
 import ReactPlayer from 'react-player'
-//const items[];
- 
-  export default function Duyurular() { 
+
+export default function Duyurular() {
     const [tutorials, setTutorials] = useState([]);
     const [currentTutorial, setCurrentTutorial] = useState(null);
-    const [duyuru, setDuyuru] = useState([]); 
+    const [duyuru, setDuyuru] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(-1);
     const [searchTitle, setSearchTitle] = useState("");
     const [controller, dispatch] = useSoftUIController();
@@ -38,7 +37,9 @@ import ReactPlayer from 'react-player'
     useEffect(() => {
 
         retrieveDuyuru();
-    }, []); 
+    }, []);
+
+
 
     const retrieveDuyuru = () => {
         DuyuruDataService.getAll()
@@ -49,7 +50,17 @@ import ReactPlayer from 'react-player'
             .catch(e => {
                 console.log(e);
             });
-    }; 
+    };
+
+
+
+
+    const setActiveTutorial = (tutorial, index) => {
+        setCurrentTutorial(tutorial);
+        setCurrentIndex(index);
+    };
+
+
     return (
         <div>
             <div className="main-wrapper">
@@ -84,7 +95,7 @@ import ReactPlayer from 'react-player'
 
             </div>
             <div className="page-head">
-                {/*  <div className="container">
+               {/*  <div className="container">
                     <div className="col-md-9">
                         <h3>Standard Post Format with preview picture</h3>
                         <span className="post-meta">Posted 22.06.2014 at 18:00h in Healthy lifestyle by <a href="#">The Ronins</a>   /   68 Likes   /   <a href="#">2 Comments</a></span>
@@ -97,25 +108,25 @@ import ReactPlayer from 'react-player'
                 </div> */}
             </div>
             <div className="blog-content">
-                <div className="container">
-                    {duyuru.map(item => (
-                        <div key={item.id} className="col-md-9">
-                            <article>
-                                <img src={item.Resim} className="img-responsive" alt="" />
-                                <div className="bottom-space-30"></div>
-                                <div className="clearfix"></div>
-
-                                <div className="col-xs-12"><p style={{ textAlign: "center" }}>{item.baslik}</p></div>
-                                <div className="col-xs-12"><p style={{ textAlign: "center" }}>Başlangıç Tarihi : {item.baslangicTarihi}-{item.bitisTarihi}</p></div>
-
-                                <div className="bottom-space-30"></div>
-                                <div className="clearfix"></div>
-                                <div className="bottom-space-30"></div>
-                                <div className="clearfix"></div>
-                                <p>{item.icerik}</p>
-                                <div className="bottom-space-30"></div>
-                                <div className="clearfix"></div>
-                                {/* <div className="row">
+        <div className="container">
+        {duyuru.map(item => (
+            <div key={item.id} className="col-md-9">
+                <article>
+                    <img src={item.Resim} className="img-responsive" alt=""/>
+                    <div className="bottom-space-30"></div>
+                    <div className="clearfix"></div>
+                    
+                    <div className="col-xs-12"><p style={{  textAlign: "center" }}>{item.baslik}</p></div>
+                    <div className="col-xs-12"><p style={{  textAlign: "center" }}>Başlangıç Tarihi : {item.baslangicTarihi}-{item.bitisTarihi}</p></div>
+                    
+                    <div className="bottom-space-30"></div>
+                    <div className="clearfix"></div>                   
+                    <div className="bottom-space-30"></div>
+                    <div className="clearfix"></div>
+                    <p>{item.icerik}</p>
+                    <div className="bottom-space-30"></div>
+                    <div className="clearfix"></div>
+                    {/* <div className="row">
                         <div className="col-md-7">
                             <p className="bold">Caveats worth mentioning</p>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu ante eget nisl convallis tempus. Phasellus ante lectus, tincidunt tincidunt dui a, rhoncus interdum est. Sed molestie quis augue ac pulvinar. Pellentesque egoists sed tortor egestas pretium. Nam eget fermentum tellus, et fermentum diam. Mauris hendrerit, diam non commodo laoreet, est elit volutpat mauris, vel vehicula nisl orci id nibh. Pellentesque mollis convallis condimentum.</p>
@@ -141,7 +152,7 @@ import ReactPlayer from 'react-player'
                         <li><a href="#">Doctors</a></li>
                         <li><a href="#">Disease</a></li>
                     </ul> */}
-                                {/*  <div className="sharepost">
+                   {/*  <div className="sharepost">
                         <div className="row">
                             <div className="col-md-6">
                                 <h4>Share this Post</h4>
@@ -157,7 +168,7 @@ import ReactPlayer from 'react-player'
                             </div>
                         </div>
                     </div> */}
-                                {/* <div className="author-info">
+                    {/* <div className="author-info">
                         <img src={author} alt="" className="img-responsive"/>
                         <h5><em>Author:</em> The Ronins</h5>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu ante eget nisl convallis tempus. Phasellus ante lectus, tincidunt tincidunt dui a, rhoncus interdum est. Sed molestie quis augue ac pulvinar. Pellentesque egoists sed tortor egestas pretium. Nam eget fermentum tellus, et fermentum diam.</p>
@@ -187,8 +198,8 @@ import ReactPlayer from 'react-player'
                             </ul>
                         </div>
                     </div> */}
-                            </article>
-                            {/*     <div className="comments">
+                </article>
+            {/*     <div className="comments">
                     <h4>Post has 3 comments</h4>
                     <ul>
                         <li>
@@ -214,7 +225,7 @@ import ReactPlayer from 'react-player'
                         </li>
                     </ul>
                 </div> */}
-                            {/* <div className="comment-form">
+                {/* <div className="comment-form">
                     <h4>Leave a comment</h4>
                     <form id="comment-form">
                         <div className="row">
@@ -243,9 +254,9 @@ import ReactPlayer from 'react-player'
                         </div>
                     </form>
                 </div> */}
-                        </div>))}
-                    <aside className="col-md-3">
-                        {/*   <div className="side-content">
+            </div>))}
+            <aside className="col-md-3">
+              {/*   <div className="side-content">
                     <h5>Main Categories</h5>
                     <ul className="cat">
                         <li>
@@ -259,14 +270,15 @@ import ReactPlayer from 'react-player'
                         </li>
                     </ul>
                 </div> */}
-                        <div className="side-content">
-                            <div style={{ width: "500px", height: "950px" }}>
-                         {/*    <Chrono items={items} /> */}
-
-                         <Timeline />
-                            </div>
-                        </div>
-                        {/* <div className="side-content">
+                {/* <div className="side-content">
+                    <h5>KURUMSAL</h5>
+                    <ul className="list1">
+                    <li><Link to={"/Tarihce"} className="nav-link">TARİHÇE</Link></li>
+                    <li><Link to={"/Degerler"} className="nav-link">DEĞERLER</Link></li>
+                    <li><Link to={"/SosyalSorumluluk"} className="nav-link">SOSYAL SORUMLULUK</Link></li>
+                    </ul>
+                </div> */}
+                {/* <div className="side-content">
                     <h5>Join the newsletter</h5>
                     <p>Join the 1000+ others and subscribe. We promise You won't recive any spam from us!</p>
                     <form className="side-newsletter">
@@ -306,32 +318,32 @@ import ReactPlayer from 'react-player'
                         <li><a href="#">Schedule</a></li>
                     </ul>
                 </div> */}
-                    </aside>
-                </div>
-            </div>
-
-            <div className="footer2">
-                <img src={logo2} alt="" />
-            </div>
-
-            <div className="footer2-bottom">
-                <div className="container">
-                    <div className="col-md-6">
-                        <p>Copyright 2014. <b>HEARTIFY</b>. All Rights Reserved.</p>
-                    </div>
-                    <div className="col-md-6">
-                        <ul className="footer-social">
-                            <li><a href="#"><i className="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i className="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i className="fa fa-google-plus"></i></a></li>
-                            <li><a href="#"><i className="fa fa-pinterest"></i></a></li>
-                            <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
-                        </ul>
-                    </div>
-                    <a href="javascript:void(0)" className="bttop"><img src={backtotop} alt="" /></a>
-                </div>
-            </div>
+            </aside>
         </div>
+    </div>
+    
+    <div className="footer2">
+        <img src={logo2} alt=""/>
+    </div>
+    
+    <div className="footer2-bottom">
+        <div className="container">
+            <div className="col-md-6">
+                <p>Copyright 2014. <b>HEARTIFY</b>. All Rights Reserved.</p>
+            </div>
+            <div className="col-md-6">
+                <ul className="footer-social">
+                    <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i className="fa fa-google-plus"></i></a></li>
+                    <li><a href="#"><i className="fa fa-pinterest"></i></a></li>
+                    <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+            <a href="javascript:void(0)" className="bttop"><img src={backtotop} alt=""/></a>
+        </div>
+    </div>
+</div>
 
     )
 
