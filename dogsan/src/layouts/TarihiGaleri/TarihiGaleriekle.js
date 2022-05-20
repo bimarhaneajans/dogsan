88d0be4ffcc53a98b764f461d34eb73d2cd20a1f
nur81 <100799851+nur81@users.noreply.github.com>
2022-05-21 02:29:49 +0300

    dd
    ddd

--- dogsan/src/layouts/TarihiGaleri/TarihiGaleriekle.js
+++ dogsan/src/layouts/TarihiGaleri/TarihiGaleriekle.js
@@ -193,7 +193,7 @@ export default class UploadImages extends Component {
           </div>
         )}
 
-        <div className="card mt-3">
+      {/*   <div className="card mt-3">
           <div className="card-header">List of Files</div>
           <ul className="list-group list-group-flush">
             {imageInfos &&
@@ -204,7 +204,7 @@ export default class UploadImages extends Component {
                 </li>
               ))}
           </ul>
-        </div>
+        </div> */}
         
       </div>
       </div>
