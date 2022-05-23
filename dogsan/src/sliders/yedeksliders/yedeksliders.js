1a009448621fd87e1200ba66560f0406d2322441
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2022-05-23 18:05:21 +0300

    dd
    ddd

--- dogsan/src/sliders/yedeksliders/yedeksliders.js
+++ dogsan/src/sliders/yedeksliders/yedeksliders.js
@@ -1,5 +1,7 @@
 import "../yedeksliders/style.css";
 import React, { Component } from 'react'
+ import VideoPlayer from 'react-video-js-player';
+
 var classNames = require('classnames');
 
 
@@ -13,8 +15,16 @@ export default class CitiesSlider extends React.Component {
       this.AUTOCHANGE_TIME = 4000;
       
       this.state = { activeSlide: -1, prevSlide: -1, sliderReady: false };
+
+      
     }
-    
+     player = {}
+    state = {
+        video: {
+            src: "http://www.example.com/path/to/video.mp4",
+            poster: "http://www.example.com/path/to/video_poster.jpg"
+        }
+    }
     componentWillUnmount() {
       window.clearTimeout(this.changeTO);
     }
@@ -42,15 +52,58 @@ export default class CitiesSlider extends React.Component {
       if (activeSlide >= length) activeSlide = 0;
       this.setState({ activeSlide, prevSlide });
     }
-    
+    onPlayerReady(player){
+      console.log("Player is ready: ", player);
+      this.player = player;
+  }
+
+  onVideoPlay(duration){
+      console.log("Video played at: ", duration);
+  }
+
+  onVideoPause(duration){
+      console.log("Video paused at: ", duration);
+  }
+
+  onVideoTimeUpdate(duration){
+      console.log("Time updated: ", duration);
+  }
+
+  onVideoSeeking(duration){
+      console.log("Video seeking: ", duration);
+  }
+
+  onVideoSeeked(from, to){
+      console.log(`Video seeked from ${from} to ${to}`);
+  }
+
+  onVideoEnd(){
+      console.log("Video ended");
+  }
     render() {
       const { activeSlide, prevSlide, sliderReady } = this.state;
       return (
         <div className={classNames('slider', { 's--ready': sliderReady })}>
            <div className="slider__slides">
             {this.props.slaty.map((slaty, index) => (
-             console.log(slaty),
-              <div
+            
+            
+            console.log(slaty),
+            <VideoPlayer
+            controls={true}
+            src={this.state.video.src}
+            poster={this.state.video.poster}
+            width="720"
+            height="420"
+            onReady={this.onPlayerReady.bind(this)}
+            onPlay={this.onVideoPlay.bind(this)}
+            onPause={this.onVideoPause.bind(this)}
+            onTimeUpdate={this.onVideoTimeUpdate.bind(this)}
+            onSeeking={this.onVideoSeeking.bind(this)}
+            onSeeked={this.onVideoSeeked.bind(this)}
+            onEnd={this.onVideoEnd.bind(this)}
+        />
+           /*   <div
                 className={classNames('slider__slide', { 's--active': activeSlide === index, 's--prev': prevSlide === index  })}
                 key={slaty.ismi}
                 >
@@ -68,7 +121,7 @@ export default class CitiesSlider extends React.Component {
                     </div>
                   ))}
                 </div>
-              </div>
+              </div> */
             ))}
           </div>
           <div className="slider__control" onClick={() => this.changeSlides(-1)} />
