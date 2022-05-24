46d61f335d983d9256fafa27a99fa8e994c146e0
nur81 <100799851+nur81@users.noreply.github.com>
2022-05-24 21:37:15 +0300

    son düzeltilmiş
    d

--- dogsan/src/sliders/yedeksliders.js
+++ dogsan/src/sliders/yedeksliders.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useMemo, useRef } from "react";
 import { Carousel } from "react-bootstrap"; 
 import ReactPlayer from "react-player";
-/* import "bootstrap/dist/css/bootstrap.css";
-import "./VideoCarousel.css";  */
+/*  import "bootstrap/dist/css/bootstrap.css"; */
+  import "./bootstrap.slider.css" 
+/* import "./VideoCarousel.css";  */
 import SliderService from "../services/SliderService";
 import CitiesSlider from './CitiesSlider.js';
 
