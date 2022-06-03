442b87ba1eb23bc8777a1fb9fb3aee1bed18b401
nur81 <100799851+nur81@users.noreply.github.com>
2022-06-03 10:46:05 +0300

    fff
    fff

--- dogsan/src/layouts/Duyurular/Duyurular.js
+++ dogsan/src/layouts/Duyurular/Duyurular.js
@@ -117,14 +117,14 @@ export default function Duyurular() {
                                 <div className="bottom-space-30"></div>
                                 <div className="clearfix"></div>
 
-                                <div className="col-xs-12"><p style={{ textAlign: "center" }}>  <div dangerouslySetInnerHTML={{ __html:DOMPurify.sanitize(JSON.parse(item.baslik)) }}  ></div></p></div>
-                                <div className="col-xs-12"><p style={{ textAlign: "center" }}>Başlangıç Tarihi :  <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(JSON.parse(item.Tarih)) }}  ></div></p></div>
+                                <div className="col-xs-12"><p style={{ textAlign: "center" }}>  <div dangerouslySetInnerHTML={{ __html:item.baslik}}  ></div></p></div>
+                                <div className="col-xs-12"><p style={{ textAlign: "center" }}>Başlangıç Tarihi :  <div dangerouslySetInnerHTML={{ __html: item.Tarih}}  ></div></p></div>
 
                                 <div className="bottom-space-30"></div>
                                 <div className="clearfix"></div>
                                 <div className="bottom-space-30"></div>
                                 <div className="clearfix"></div>
-                                <p style={{ textAlign: "center" }} >  <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(JSON.parse(item.icerik)) }}  ></div></p>
+                                <p style={{ textAlign: "center" }} >  <div dangerouslySetInnerHTML={{ __html:item.icerik }}  ></div></p>
                                 
                                 
                                {/*  <React.Fragment>
